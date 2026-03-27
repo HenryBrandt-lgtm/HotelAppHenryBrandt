daf1dc629bee4025993308f17a17cc1ca72d3d4e
Henry Brandt <henry_brandt@hotmail.com>
2026-03-27 10:50:05 +0100

    note to slef

--- HotelApp/Program.cs
+++ HotelApp/Program.cs
@@ -15,6 +15,7 @@ namespace HotelApp
             //I bokning när man väljer kund att checka in borde man kunna skapa en kund där...!
             //Lägga till en README FIIL
 
+            //I datainitilicer där jag seedar. Kan jag göra det till listor där jag jämför flera saker, dvs kan korta ner allt?
         }
     }
 }
