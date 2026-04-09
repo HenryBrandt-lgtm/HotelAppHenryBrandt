ef42765592aa0c8548d3a811308e84ada1224c0b
Henry Brandt <henry_brandt@hotmail.com>
2026-04-09 10:13:40 +0200

    .

--- HotelApp/Program.cs
+++ HotelApp/Program.cs
@@ -14,19 +14,19 @@ namespace HotelApp
 
             // Lägga till extrasängar vid bokning?
 
-            //I datainitilicer där jag seedar. Kan jag göra det till listor där jag jämför flera saker, dvs kan korta ner allt?
+            // I datainitilicer där jag seedar. Kan jag göra det till listor där jag jämför flera saker, dvs kan korta ner allt?
 
-            //skriv i readme
+            // email måste kolla om den redan finns. Får inte ha samma pga invoice, skicka invoice?
 
-            //email måste kolla om den redan finns. Får inte ha samma pga invoice
-
-            //Ska man kunna checka in ?
+            // Ska man kunna checka in ?
 
             // hur vill jag göra med room enums?
 
-            // lägga till avbokning som eget menyval?
-
             //hittade bugg, lägger man till kund vid bokning så börjar bokningen om
+
+            //flytta container from app.cs till egen klass?
+
+            //kolla notpad med saker som finns kvar
         }
     }
 }
